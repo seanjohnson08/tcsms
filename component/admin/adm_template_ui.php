247dace5aebd87f7278a234d80e535fd641d3347
VinnyVideo <168298103+VinnyVideo@users.noreply.github.com>
2025-05-07 19:43:27 -0400

    Update adm_template_ui.php
    PHP 7 fix, updated <br> tags

--- component/admin/adm_template_ui.php
+++ component/admin/adm_template_ui.php
@@ -10,14 +10,15 @@ class adm_template_ui {
 	//	$DB->query("SELECT mid,module_name FROM {$CFG['db_pfx']}_modules");
 	//	while ($row = $DB->fetch_row()) {
 		reset($STD->modules->module_set);
-		while (list(,$row) = each ($STD->modules->module_set)) {
+		//while (list(,$row) = each ($STD->modules->module_set)) {
+		foreach ( $STD->modules->module_set as $row ) {
 			$url = $STD->encode_url( $_SERVER['PHP_SELF'], "act=modq&param=01&c={$row['mid']}" );
-			$menus .= ":: <a href='$url'>{$row['module_name']}</a><br />\n";
+			$menus .= ":: <a href='$url'>{$row['module_name']}</a><br>\n";
 		}
 		
 		$url = $STD->encode_url( $_SERVER['PHP_SELF'], 'act=modq&param=08' );
 		$menus .= "<hr>\n";
-		$menus .= ":: <a href='$url'>Create New</a><br />\n";
+		$menus .= ":: <a href='$url'>Create New</a><br>\n";
 		
 		return $menus;
 	}
